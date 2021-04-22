4f41c71c67134a4f42a37b9d264ed5c9c3e9dc88
twt-tec <79842908+twt-tec@users.noreply.github.com>
2021-04-23 07:54:14 +0800

    Add files via upload

--- writing-master.cpp
+++ writing-master.cpp
@@ -1,3 +1,4 @@
+//4.5
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 #include <windows.h>
@@ -5,13 +6,112 @@
 #include <io.h>
 //#define txet cout<<"get here!";Sleep(1000);
 using namespace std;
-const int N=100;
-int n,z_choose;
+const int N=600;
+int n;
 struct node
 {
         string totEh,Eh[N],Ch;
         int cntEh;
 } word[N];
+namespace twy
+{
+typedef struct Frame
+{
+        COORD position[2];
+        int flag;
+}Frame;
+
+void SetPos(COORD a)
+{
+        HANDLE out=GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleCursorPosition(out, a);
+}
+
+void SetPos(int i, int j)
+{
+        COORD pos={i, j};
+        SetPos(pos);
+}
+/*=============== 画面 ===============*/
+
+void drawRow(int x, int y1, int y2, char ch)    //把第x行，[y1, y2] 之间的坐标填充为 ch
+{
+        SetPos(y1,x);
+        for(int i=0; i<=(y2-y1+1); i++)
+                printf("%c",ch);
+}
+void Draw_opt(int size,string s[])
+{
+        SetPos(2,6);
+        cout<<">>";
+        for(int i=1; i<=size; i++)
+        {
+                SetPos(5,2*i+4);
+                cout<<i<<". "<<s[i];
+        }
+}
+void Draw_opt(int size,vector<string> s)
+{
+        SetPos(2,6);
+        cout<<">>";
+        for(int i=1; i<=size; i++)
+        {
+                SetPos(5,2*i+4);
+                cout<<i<<". "<<s[i-1];
+        }
+}
+void Draw(int n)
+{
+        switch (n)
+        {
+        case -1:    //====初始化
+        {
+                system("cls");
+                SetPos(15, 0);
+                printf("默 写 大 师");
+                SetPos(15, 2);
+                printf("按'w'和's'选择,回车确定");
+                drawRow(1, 0, 40, '-');
+                drawRow(3, 0, 40, '-');
+                puts("");
+                break;
+        }
+        }
+}
+int drawp(int l,int r,int &j,int m)
+{
+        int x=getch();
+        if(x==-32) x=getch();
+        SetPos(2,2*j+4);
+        printf("  ");
+        if(x=='w'|| x=='W' || x==72)
+        {
+                j--;
+                if(j==l-1) j=r;
+                SetPos(2,2*j+m);
+                printf(">>");
+        }
+        else if(x=='s'|| x=='S'|| x==80)
+        {
+                j++;
+                if(j==r+1) j=l;
+                SetPos(2,2*j+m);
+                printf(">>");
+        }
+        else if(x==13)//谁再用'k'，谁死马
+        {
+                Draw(-1);
+                return j;
+        }
+        else
+        {
+                SetPos(2,2*j+4);
+                printf(">>");
+        }
+        return -1;
+}
+}
+using namespace twy;
 namespace zm
 {
 void choose_pattern(char a[]);
@@ -61,7 +161,7 @@ void create(char a[])
         Thesaurus<<"最后一行要换行(其余详见使用手册)\n";
         Thesaurus.close();
         system(name);
-        system("cls");
+        getchar();
 }
 void getFiles(string path,string exd,vector<string>&files)
 {
@@ -80,7 +180,7 @@ void getFiles(string path,string exd,vector<string>&files)
                 _findclose(hFile);
         }
 }
-string GetExePath()     //获取当前文件夹路径
+string GetExePath()       //获取当前文件夹路径
 {
         char szFilePath[MAX_PATH+1]={0};
         GetModuleFileNameA(NULL,szFilePath,MAX_PATH);
@@ -90,69 +190,41 @@ string GetExePath()     //获取当前文件夹路径
 }
 void open(char a[])
 {
-        puts("已有词库：");
         string argv=GetExePath();
-        vector<string>files;
+        vector<string> files;
         //获取该路径下的所有.in .txt文件
 //	    getFiles(argv,"in",files);
         getFiles(argv,"txt",files);
-        int size=files.size(); //总共文件数
-        for (int i=0; i<size; i++)
-                cout<<i+1<<" ->"<<files[i].c_str()<<endl;
-        Color(8); printf("%d -> back\n",size+1); Color(7);
-        printf("输入数字，选择词库:");
-        int choose;
-        cin>>choose;
-        if (choose==size+1)
-        {
-                return Custom(a);
-        }
-        if (choose>size+1 || choose<=0)
-        {
-                system("cls");
-                puts("请输入所给范围的数字！");
-                Sleep(888);
-                system("cls");
-                open(a);
-                return;
-        }
-        choose-=1;
-        //string -> char[]
+        files.push_back("Back");
+        int size=files.size();  //总共文件数
+        Draw_opt(size,files);
+        int choose=1;
+        while(drawp(1,size,choose,4)==-1);
+        choose--;
+        if(choose==size-1) return Custom(a);
         string tem=files[choose].c_str();
         for (int i=0; i<tem.size(); i++)
                 a[i]=files[choose].c_str()[i];
-        printf("已选择%s\n按任意键继续",a);
-        char temp=getchar();
 }
 
 void edit_choose(char in[])
 {
-        system("cls");
-        cout<< "已选中 "<<in<<endl;
-        printf("1 ->删除\n");
-        printf("2 ->编辑词库\n");
-        Color(8); printf("3 ->back\n"); Color(7);
-        int choose;
-        cin>>choose;
-        if (choose==3)
+        Draw(-1);
+        string opt[10]=
         {
-                return;
-        }
-        if (choose>3 || choose<=0)
-        {
-                system("cls");
-                puts("请输入所给范围的数字！");
-                Sleep(888);
-                system("cls");
-                edit_choose(in);
-                return;
-        }
+                "",
+                "删除",
+                "编辑词库",
+                "Back"
+        };
+        Draw_opt(3,opt);
+        int choose=1;
+        while(drawp(1,3,choose,4)==-1);
         if (choose==1)
         {
                 system("cls");
                 printf("删除中...\n");
                 remove(in);
-                Sleep(666);
                 printf("删除成功!!!\n");
                 Sleep(666);
                 return;
@@ -162,90 +234,115 @@ void edit_choose(char in[])
                 system("cls");
                 printf("进入中...\n");
                 system(in);
-                Sleep(666);
                 printf("编辑成功!!!\n");
                 Sleep(666);
                 return;
         }
-}
-void edit(char a[])
-{
-        system("cls");
-        puts("无法修改系统自带词库");
-        puts("仅可修改用户自行添加词库");
-        puts("已有词库：");
-        string argv=GetExePath();
-        vector<string>files;
-        //获取该路径下的所有.in .txt文件
-//	    getFiles(argv,"in",files);
-        getFiles(argv,"txt",files);
-        int size=files.size(); //总共文件数
-        for (int i=0; i<size; i++)
-                cout<<i+1<<" ->"<<files[i].c_str()<<endl;
-        Color(8); printf("%d -> back\n",size+1); Color(7);
-        printf("输入需要编辑的词库的编号:");
-        int choose;
-        cin>>choose;
-        if (choose==size+1)
+        else if (choose==3)
         {
-                return Custom(a);
+                return;
         }
-        if (choose>size+1 || choose<=0)
+        else
         {
                 system("cls");
                 puts("请输入所给范围的数字！");
                 Sleep(888);
                 system("cls");
-                edit(a);
+                edit_choose(in);
                 return;
         }
+}
+void edit(char a[])
+{
+        string argv=GetExePath();
+        vector<string>files;
+        //获取该路径下的所有.in .txt文件
+//	    getFiles(argv,"in",files);
+        getFiles(argv,"txt",files);
+        files.push_back("back");
+        int size=files.size();  //总共文件数
+        Draw(-1);
+        Draw_opt(size,files);
+        int choose=1;
+        while(drawp(1,size,choose,4)==-1);
+        if (choose==size) return Custom(a);
         choose--;
         char tem[100]="";
         string_char(files[choose].c_str(),tem);
         edit_choose(tem);
         edit(a);
 }
-
 void Custom(char a[])
 {
-        system("cls");
-        printf("1 ->创造新词库\n");
-        printf("2 ->打开已有词库,开始练习\n");
-        printf("3 ->编辑已有词库\n");
-        Color(8); printf("4 ->back\n"); Color(7);
-        printf("请选择:");
-        char choose;
-        cin>>choose;
+        Draw(-1);
+        string opt[10]=
+        {
+                "",
+                "创造新词库",
+                "打开已有词库",
+                "编辑已有词库",
+                "Back"
+        };
+        Draw_opt(4,opt);
+        int choose=1;
+        while(drawp(1,4,choose,4)==-1);
         switch(choose)
         {
-        case '1': system("cls"); create(a); z_choose=1; return;
-        case '2': system("cls"); open(a); z_choose=2; return;
-        case '3': system("cls"); edit(a); return;
-        case '4': system("cls"); choose_pattern(a); return;
-        default: system("cls"); puts("请输入所给范围的数字！"); Sleep(1000); system("cls"); Custom(a); break;
+        case 1: system("cls"); create(a); return;
+        case 2: open(a); return;
+        case 3: system("cls"); edit(a); return;
+        case 4: system("cls"); choose_pattern(a); return;
         }
+}
+void Own_Thesaurus(char a[])
+{
+        string argv=GetExePath();
+        vector<string> files;
+        //获取该路径下的所有.in .txt文件
+        getFiles(argv,"in",files);
+//        getFiles(argv,"txt",files);
+        files.push_back("Back");
+        int size=files.size();  //总共文件数
+        Draw_opt(size,files);
+        int choose=1;
+        while(drawp(1,size,choose,4)==-1);
+        choose--;
+        if(choose==size-1) return choose_pattern(a);
+        string tem=files[choose].c_str();
+        for (int i=0; i<tem.size(); i++)
+                a[i]=files[choose].c_str()[i];
+}
+void morre()
+{
+        puts("了解更多twt-tec软件资讯请进入我们的主页：");
+        puts("https://twt-tec.github.io/");
+        Sleep(1500);
+        system("start https://twt-tec.github.io/");
+        Sleep(3000);
 }
 void choose_pattern(char a[])
 {
         system("cls");
-        Color(8); puts("欢迎使用 writing master"); Color(7);
-        Sleep(666);
-        system("cls");
-        puts("主页:");
-        puts("1 ->开始练习自带词库");
-        puts("2 ->自定义词库");
-        puts("3 ->使用手册");
-        Color(8); puts("4 ->退出"); Color(7);
-        printf("请选择:");
-        char choose;
-        cin>>choose;
+        Draw(-1);
+        string opt[10]=
+        {
+                "",
+                "【官方词库】             ",
+                "【自定义词库】         ",
+                "【使用手册】             ",
+                "【更多】             ",
+                "【退出】"
+        };
+        Draw_opt(5,opt);
+        int choose=1;
+        while(drawp(1,5,choose,4)==-1);
         switch(choose)
         {
-        case '1': z_add("bx1unit",a); z_choose=0; system("cls"); return;
-        case '2': system("cls"); Custom(a); return;
-        case '3': system("cls"); puts("进入中..."); system("使用手册.pdf"); choose_pattern(a); return;
-        case '4': exit(0);
-        default: system("cls"); puts("请输入所给范围的数字！"); Sleep(1000); choose_pattern(a); break;
+        case 1: Own_Thesaurus(a); system("cls"); return;
+        case 2: Custom(a); return;
+        case 3: system("cls"); puts("进入中..."); system("使用手册.pdf"); choose_pattern(a); return;
+        case 4: system("cls"); morre(); choose_pattern(a); return;
+        case 5: system("cls"); exit(0);
         }
 }
 }
@@ -258,7 +355,6 @@ void add(char temp[],char into[])
 }
 void initialization()    //初始化
 {
-        z_choose=0;
         for (int i=0; i<=n; i++)
                 for (int j=0; j<N; j++)
                         word[i].Eh[j]="",word[i].cntEh=0;
@@ -267,18 +363,7 @@ void main()
 {
         char unit[N]="",ordinal[N]="";
         zm::choose_pattern(unit);
-        if (z_choose==0)
-        {
-                cout<<"输入要默写的单元"<<"\n"<<unit;
-                scanf("%s",ordinal);
-                getchar();
-                add(ordinal,unit);
-                add(".in",unit);
-        }
-        else if (z_choose==1||z_choose==2)
-        {
-                char tem=getchar();
-        }
+
         ifstream read;
         read.open(unit);
         read>>n;
@@ -338,7 +423,7 @@ void Usual(int level,int T,int F)
 {
         printf("进度: %d/%d\n",level,n);
         printf("正确率:%.1lf% (%d/%d)\n",T*100.0/(T+F),T,T+F);
-        puts("输入'>>'可跳过当前单词,输入'.'结束测试");
+        puts("输入'>>'跳过单词,'?'去除单词,'.'结束测试");
 }
 }
 namespace work
@@ -395,7 +480,8 @@ void Selection()
                 pick[r]=0;
                 while(!check())
                 {
-                        if(init==">>") {cout<<word[r].Eh[1]<<endl; Sleep(500); return;}
+                        if(init==">>") {cout<<word[r].Eh[1]<<endl; Sleep(666); return;}
+                        if(init=="?") return (void)(n--,Sleep(333),pick[r]=1);
                         if(init==".") {Finish(false); return;}
                         flase++;
                         Interface::WrongAnswer(r);
@@ -418,10 +504,19 @@ void main()
         }
 }
 }
-int main()
+namespace window
+{
+void main()
 {
         system("chcp 936");
+        system("title writing master");//设置cmd窗口标题
+        system("mode con cols=42 lines=30");//窗口宽度高度
         system("cls");
+}
+}
+int main()
+{
+        window::main();
         init::main();
         work::main();
 }
